c4a8443f26dcb91e51b6da1ea0af62376c07de41
jjh <jehyeon@omnitel.co.kr>
2018-06-29 10:38:04 +0900

    [리펙토링/임시변수를 메서드 호출로 전환]
    임시변수를 메서드 호출로 전환 정리
    
    - 문서 작성
    - 예제 코드 작성

---
+++ example/src/main/java/me/jjh/refactoring/chapter06/ReplaceTempWithQuery/ReplaceTempWithQuery.java
@@ -0,0 +1,48 @@
+package me.jjh.refactoring.chapter06.ReplaceTempWithQuery;
+
+/**
+ * 임시변수를 메서드 호출로 전환
+ * 
+ * - 수식의 결과를 저장하는 임시변수가 있을 땐 
+ * 그 수식을 빼내어 메서드로 만든 후 , 임시변수 참조 부분을 전부 수식으로 교체하자.
+ * 새로 만든 메서드는 다른 메서드에서도 호출 가능하다.
+ * 
+ * @author jjh
+ *
+ */
+public class ReplaceTempWithQuery {
+
+	int _quanity = 10;
+	int _itemPrice = 1000;
+	
+	/**
+	 * 리펙토링 전
+	 * @return
+	 */
+	public double getPrice() {
+		int basePrice = _quanity * _itemPrice;
+		double discountFactor;
+		if(basePrice > 1000) discountFactor = 0.95;
+		else discountFactor = 0.98;
+		return basePrice * discountFactor;
+	}
+
+	/**
+	 * 리펙토링 후
+	 * - 임시변수 basePrice, discountFactor를 메서드 호출로 전환
+	 * @return
+	 */
+	public double getPrice_refactoring() {
+		return basePrice() * discountFactor();
+	}
+	
+	private int basePrice() {
+		return _quanity * _itemPrice;
+	}
+	
+	private double discountFactor() {
+		if(basePrice() > 1000) return 0.95;
+		else return 0.98;
+	}
+	
+}
